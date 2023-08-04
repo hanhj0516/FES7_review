556835e54e4967d23fe7f472bd72195ff5c2a9f9
hanhj0516 <hanhj0516@naver.com>
2023-08-04 16:12:26 +0900

    20230803-incla

---
+++ 2023.08.03/007_function_1_연습문제.js
@@ -0,0 +1,29 @@
+function add(x, y){
+    return x + y
+}
+
+add(1,2)
+
+function sub(x, y){
+    return x - y
+}
+
+function mult(x, y){
+    return x * y
+}
+
+function divide(x, y){
+    return x % y
+}
+
+https://school.programmers.co.kr/learn/courses/30/lessons/120803
+
+
+
+function addAndMult(x, y){
+    return ((x + y)*(x + y))
+}
+
+function QuadraticEquation(x) {
+    return (x**2 +4*x - 12)
+}
---
+++ 2023.08.03/009.신입사원 주의코드.js
@@ -0,0 +1,12 @@
+function test() {
+    console.log('hello')
+    console.log('hello')
+    console.log('hello')
+}
+
+function test() {
+    console.log('hello')
+}
+
+test() // 뒤에 함수가 덮어써서 hello 한번만 출력
+
