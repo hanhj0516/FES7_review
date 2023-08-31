a1e8b9c39520eb7a6a0e4d9bcea3a37290dc1b2f
hanhj0516 <hanhj0516@naver.com>
2023-08-31 18:08:27 +0900

    20230831-rev

---
+++ 2023.08.31/king.js
@@ -0,0 +1,15 @@
+{/* <div id="myDiv">Hello, World!</div>
+이 요소의 텍스트 콘텐츠를 변경하려고 합니다.
+로직을 말하거나 코드를 써주세요. */}
+
+document.querySelector(#myDiv)
+myDiv.textContent = 'Hi'
+
+document.querySelector(form).value
+
+
+
+// 15.요소의 크기 및 위치 정보를 제공하는 DOM 메소드는 무엇인가요?
+getBoundingClientRect()
+
+
