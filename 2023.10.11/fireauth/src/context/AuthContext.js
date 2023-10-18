f90e37f6e1898c00f11caed0a4e41f72b1562187
hanhj0516 <hanhj0516@naver.com>
2023-10-19 01:50:14 +0900

    20231019-커밋메세지-test

---
+++ 2023.10.11/fireauth/src/context/AuthContext.js
@@ -0,0 +1,23 @@
+import { createContext, useReducer } from "react";
+
+const AuthContext = createContext();
+
+const authReducer = (state, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
+
+const AuthContextProvider = ({ children }) => {
+
+    const [state, dispatch] = useReducer(authReducer, { user: null })
+
+    return (
+        <AuthContext.Provider value={{ ...state, dispatch }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export { AuthContext, AuthContextProvider }
